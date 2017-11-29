bc6afb95bea0defb78cd6e8b5ea9720bafe383c6
cihan erman <cihan.erman@hotmail.com>
2017-11-29 11:19:12 +0300

    sınav

---
+++ Content/kullanicilar.js
@@ -0,0 +1,28 @@
+﻿ [
+     {
+         "id": "0",
+        "kullanici_adi":"anatolian",
+        "sifre": "stream",
+        "isim": "Muhammed",        
+        "soyisim": "Söylemez",
+        "profil_resmi": "img/flowers.jpg",
+        "d_tarihi": "21.01.1999",
+        "mezun_oldgu_okul": "İskenderun Technical University",
+        "fakulte": "Engineering",
+        "ozgecmis": "The Greatest form of control is where you think you’re free, when you’re being fundamentally manipulated and dictated to.One form of dictatorship is being in a prison cell and you can see the bars and touch it.The other one is sitting in a prison cell but you can’t see the bars so you think you’re freeWe are in a country and in a world that is being run by unbelievebly sick people. The chazm what we are told is going on but what is really going on is absloutly anormus",
+        "konular": ["C#", "Javascript", "Html", "Arduino","JQuery","Petroleum","Big Data", "SQL"]
+    },
+     {
+        "id": "1",
+        "kullanici_adi": "cihan",
+        "sifre": "123",
+        "isim": "Cihan",
+        "soyisim": "ERMAN",
+        "profil_resmi": "content/cihan.jpg",
+        "d_tarihi": "01.01.1985",
+        "mezun_oldgu_okul": "Gazi Üni",
+        "fakulte": "ESEF",
+        "ozgecmis": "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit",
+        "konular": ["C#", "Javascript", "Html","Arduino"]
+    }
+]
